173dc428e6ce901e7f223d218e8dd6321924d71d
DidierVR <didier_vanreeth@yahoo.co.uk>
2017-06-27 21:47:24 +0200

    Read data works - program starts - factory is set up - all data form table simpleclass s read - data printout - program terminates ok

--- src/connecthibernate/SimpleClass.java
+++ src/connecthibernate/SimpleClass.java
@@ -10,32 +10,42 @@ package connecthibernate;
  * @author Didier
  */
 public class SimpleClass {
-	private int id;
-	private String name;
-	private double value;
-	
-	public int getId() {
-		return id;
-	}
-	
-	public void setId(int id) {
-		this.id = id;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public double getValue() {
-		return value;
-	}
-	
-	public void setValue(double value) {
-		this.value = value;
-	}
-	
+
+    private int id;
+    private String name;
+    private double value;
+
+    public SimpleClass() {
+    }
+
+    public SimpleClass(int id, String name, double value) {
+        this.id = id;
+        this.name = name;
+        this.value = value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
 }
